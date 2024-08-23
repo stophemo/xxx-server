8e283a6706c6ba2ec957169db146e2bcb692da8d
huojie <stophemo@outlook.com>
2024-08-23 10:07:57 +0800

    返回结果统一包装

--- src/main/java/com/stp/xxx/XxxApplication.java
+++ src/main/java/com/stp/xxx/XxxApplication.java
@@ -1,15 +1,11 @@
 package com.stp.xxx;
 
-import cn.dev33.satoken.SaManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class XxxApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(XxxApplication.class, args);
-        System.out.println("Sa-Token启动成功，配置如下：" + SaManager.getConfig());
     }
-
 }
