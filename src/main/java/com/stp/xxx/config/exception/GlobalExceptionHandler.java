da890afde9c67f86cf2a6788a568b9469071dcc4
huojie <stophemo@outlook.com>
2024-11-25 10:46:25 +0800

    refactor(exception): 优化参数校验异常处理逻辑
    - 修改方法名以准确反映处理的异常类型
    - 提取所有字段错误信息，拼接为字符串返回
    - 使用 stream API 处理错误信息，提高代码可读性

--- src/main/java/com/stp/xxx/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/stp/xxx/config/exception/GlobalExceptionHandler.java
@@ -4,24 +4,35 @@ import cn.dev33.satoken.exception.NotLoginException;
 import com.stp.xxx.config.result.ResultEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.dromara.hutool.core.text.StrUtil;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice(basePackages = "com.stp.xxx")
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+@ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseBody
-    public ResultEntity<?> handleNullPointerException(MethodArgumentNotValidException e) {
+    public ResultEntity<?> handleValidationException(MethodArgumentNotValidException e) {
         log.error("参数校验异常：", e);
-        return ResultEntity.error(ErrorCodeEnum.INVALID_PARAMETER.getCode(), Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+
+        // 提取所有字段错误信息
+        List<String> errorMessages = e.getBindingResult().getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        // 拼接错误信息为字符串，或直接返回错误列表
+        String errorMessage = String.join("; ", errorMessages);
+        return ResultEntity.error(ErrorCodeEnum.INVALID_PARAMETER.getCode(), errorMessage);
     }
 
     @ExceptionHandler(BusinessException.class)
