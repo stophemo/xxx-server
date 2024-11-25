9c200cd439d7249bbf151bee624d2c837c234eb2
huojie <stophemo@outlook.com>
2024-11-25 10:35:46 +0800

    feat(exception): 添加参数校验异常处理
    - 在 GlobalExceptionHandler 中新增 MethodArgumentNotValidException 异常处理方法
    - 该处理方法将返回包含错误代码和字段验证消息的 ResultEntity 对象
    - 更新 ErrorCodeEnum，为 INVALID_PARAMETER错误码添加分号

--- src/main/java/com/stp/xxx/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/stp/xxx/config/exception/GlobalExceptionHandler.java
@@ -3,13 +3,26 @@ package com.stp.xxx.config.exception;
 import cn.dev33.satoken.exception.NotLoginException;
 import com.stp.xxx.config.result.ResultEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.dromara.hutool.core.text.StrUtil;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Slf4j
 @RestControllerAdvice(basePackages = "com.stp.xxx")
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResultEntity<?> handleNullPointerException(MethodArgumentNotValidException e) {
+        log.error("参数校验异常：", e);
+        return ResultEntity.error(ErrorCodeEnum.INVALID_PARAMETER.getCode(), Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage());
+    }
 
     @ExceptionHandler(BusinessException.class)
     @ResponseBody
@@ -24,6 +37,7 @@ public class GlobalExceptionHandler {
         log.error("未登录：", e);
         return ResultEntity.error(ErrorCodeEnum.USER_TOKEN_EXPIRED.getCode(), ErrorCodeEnum.USER_TOKEN_EXPIRED.getMessage(), e.getCause());
     }
+
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
